050a18581cca736e8ed3fd2f2d244a0792db63c0
Noxim <robin.pitteloud@unil.ch>
2016-05-04 10:18:25 +0200

    Premières versions du calendrier + liste d'objets

--- projetProginter.js
+++ projetProginter.js
@@ -4,6 +4,8 @@ archives = new Mongo.Collection('archives');
 evenementsEnCours = new Mongo.Collection('event');
 
 if (Meteor.isClient) {
+
+ // Meteor.subscribe('theItems');
 //helpers
   Template.pageAccueil.helpers({
 
@@ -17,6 +19,29 @@ if (Meteor.isClient) {
   Template.evenement.helpers({
     
   });
+
+  Template.formulaireCalendrier.helpers({
+
+  });
+
+  Template.formulaireListe.helpers({
+    'Item' : function (){
+      var currentUserId = Meteor.userId();
+      return items.find({createdBy: currentUserId}, {sort: {name : 1}});
+    },
+    'selectedClass': function(){
+      var itemId = this._id;
+        var selectedItem = Session.get('selectedItem');
+        if(itemId == selectedItem){
+            return "selected";
+        }
+    },
+    'showSelectedPlayer': function(){
+      var selectedItem = Session.get('selectedItem');
+      return items.findOne(selectedItem);
+    }
+  })
+
 //events
   Template.pageAccueil.events({
 
@@ -30,13 +55,56 @@ if (Meteor.isClient) {
   Template.evenement.events({
     
   });
+  Template.formulaireCalendrier.events({
+      'click .renderer' : function(){
+          alert(Session.get('selectedDay'));
+        }
+  });
+  Template.formulaireListe.events({
+    'click .item' : function (){
+      var playerId = this._id;
+      Session.set('selectedPlayer',playerId);
+    },
+    'click .remove' : function(){
+      var selectedPlayer = Session.get('selectedItem');
+        Meteor.call("removeItem", selectedItem);
+      },
+    'submit form': function(){
+      event.preventDefault();
+      var itemNameVar = event.target.itemName.value;
+      Meteor.call('insertItemData', itemNameVar);
+      document.getElementById('champNom').value='';
+    }
+
+  })
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
+ // Meteor.publish('theItems', function(){
+ //    var currentUser = this.userId
+ //    return items.find({createdBy: currentUser})
+ //  }); 
 }
 //methodes
 Meteor.methods({
-
+'insertItemData': function(playerNameVar){
+      check(playerNameVar, String);
+      var currentUserId = Meteor.userId();
+      if(currentUserId){
+        playersList.insert({
+          name: playerNameVar,
+                score: 0,
+                createdBy: currentUserId
+        })
+      }
+    },
+    'removeItem': function(selectedPlayer){
+      check(selectedPlayer, String);
+      var currentUserId = Meteor.userId();
+      if(currentUserId){
+        playersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+      }
+      
+    }
 });
